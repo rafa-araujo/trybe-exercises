83ef885c6f33a168a2016ba0c85401ce4d15599a
rafa-araujo <rafacarlosaraujo@gmail.com>
2021-05-20 18:22:22 -0300

    incluindo exercicios de introducao a array/for/forof

---
+++ bloco_4/dia_2/exemplo4.js
@@ -0,0 +1,7 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.pop();  // remove a última tarefa
+console.log(tasksList);
+tasksList.shift(); // remove a primeira tarefa
+console.log(tasksList); 
+// [ 'Tomar café', 'Reunião' ]
---
+++ bloco_4/dia_2/exemplo5.js
@@ -0,0 +1,6 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Tomar café');
+console.log(indexOfTask);
+
+// 1
