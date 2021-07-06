ae007af5262b7b734bb61411dedb07f66f1e98a6
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-06 13:53:51 -0300

    Resolução Exercício 1

---
+++ bloco_8/dia_1/pratica1.js
@@ -0,0 +1,17 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+
+const newEmployees = (employeeGenerator) => {
+  const employees = {
+    id1: employeeGenerator('Pedro Guerra'),
+    id2: employeeGenerator('Luiza Drumond'),
+    id3: employeeGenerator('Carla Paiva'), 
+  }
+  return employees;
+};
+
+const employeeGenerator = (fullName) => {
+  const email = fullName.toLowerCase().split(' ').join('_');
+  return {fullName, email: `${email}@betrybe.com`};
+};
+
+console.log(newEmployees(employeeGenerator));
