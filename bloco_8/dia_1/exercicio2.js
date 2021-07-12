205b27c561fbb74a9f9cbdf6774354a10a7d504e
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-07-12 18:51:49 -0300

    Alteração exercício 2

--- bloco_8/dia_1/exercicio2.js
+++ bloco_8/dia_1/exercicio2.js
@@ -6,11 +6,3 @@ const generatorNumber = (numberBet, sorteio) => {
 }
 
 return "Tente novamente" ou "Parabéns você ganhou"
-
-
-const numberChecker = (myNumber, number) => myNumber === number;
-const lotteryResult = (myNumber, callback) => {
-  const number = Math.floor((Math.random() * 5) + 1);
-  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
-};
-console.log(lotteryResult(2, numberChecker));
