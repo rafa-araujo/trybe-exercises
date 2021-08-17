6d6bc562000b112089fcc66b5b2700b0cdd07657
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-08-17 18:27:54 -0300

    Conteudo dia 11.1 - atualizando

---
+++ Desenvolvimento Front-end/bloco_11/dia_1/conteudo/hello-world-react/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NameDesc from './Component';
+
+function App() {
+  return <NameDesc />
+};
+
+export default App;
---
+++ Desenvolvimento Front-end/bloco_11/dia_1/conteudo/hello-world-react/src/Component.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class NameDesc extends Component {
+  render() {
+    return <div>
+      <h1>Rafael Araujo</h1>
+      <p>Futuro DevWeb Full Stack</p>
+    </div>
+  }
+}
+
+export default NameDesc;
