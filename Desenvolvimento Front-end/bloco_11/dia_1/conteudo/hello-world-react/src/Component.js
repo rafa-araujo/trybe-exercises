e5ed7b9aa7ea3099b828f99cf77f29e44d921884
Rafael Araujo <rafacarlosaraujo@gmail.com>
2021-09-02 13:28:09 -0300

    Conteudo JSX

--- Desenvolvimento Front-end/bloco_11/dia_1/conteudo/hello-world-react/src/Component.js
+++ Desenvolvimento Front-end/bloco_11/dia_1/conteudo/hello-world-react/src/Component.js
@@ -2,11 +2,13 @@ import React, { Component } from "react"
 
 class NameDesc extends Component {
   render() {
-    return <div>
-      <h1>Rafael Araujo</h1>
-      <p>Futuro DevWeb Full Stack</p>
-    </div>
-  }
-}
+    return ( 
+    <>
+      <h1>{ textJSX }</h1>
+    </>
+  )};
+};
+
+const textJSX = 'Hello, JSX';
 
 export default NameDesc;
