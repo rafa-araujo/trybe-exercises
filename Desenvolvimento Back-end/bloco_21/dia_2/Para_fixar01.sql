c9ea37834a0429d7d7d847358bbce0bd97501289
Rafael Araujo <rafacarlosaraujo@gmail.com>
2022-04-21 20:00:37 -0300

    Para fixar 01 - concluído

--- Desenvolvimento Back-end/bloco_21/dia_2/Para_fixar01.sql
+++ Desenvolvimento Back-end/bloco_21/dia_2/Para_fixar01.sql
@@ -31,17 +31,70 @@ LIMIT 100;
 
 /*4- Exiba o nome , email , id do endereço , endereço e distrito dos clientes que moram no distrito da California e que contêm "rene" em seus nomes.
      As informações podem ser encontradas nas tabelas address e customer.*/
+SELECT * FROM address;
+SELECT * FROM customer;
 
+SELECT 
+	c.first_name, c.email, c.address_id, a.address, a.district
+FROM 
+	customer AS c
+		INNER JOIN 
+	address AS a ON c.address_id = a.address_id
+WHERE
+	a.district = 'California'
+		AND c.first_name LIKE '%rene%';
 
 /*5- Exiba o nome e a quantidade de endereços dos clientes cadastrados. Ordene seus resultados por nomes de forma decrescente.
      Exiba somente os clientes ativos. As informações podem ser encontradas na tabela address e customer.*/
+SELECT * FROM address;
+SELECT * FROM customer;
 
+SELECT
+    c.first_name, 
+    COUNT(a.address) AS `quantidade de endereço`
+FROM
+	customer AS c
+INNER JOIN 
+    address AS a ON a.address_id = c.address_id
+WHERE
+    c.active = 1
+GROUP BY 
+    c.customer_id
+ORDER BY 
+    first_name DESC;
 
 /*6- Monte uma query que exiba o nome , sobrenome e a média de valor ( amount ) paga aos funcionários no ano de 2006. Use as tabelas payment e staff.
      Os resultados devem estar agrupados pelo nome e sobrenome do funcionário.*/
+SELECT * FROM payment;
+SELECT * FROM staff;
 
+SELECT 
+	s.first_name,
+    s.last_name,
+    AVG(p.amount) AS 'Média de Pagamento'
+FROM
+	staff AS s
+INNER JOIN
+	payment p ON s.staff_id = p.staff_id
+WHERE
+	YEAR(p.payment_date) = 2006
+GROUP BY 
+	s.staff_id;
 
 /*7- Monte uma query que exiba o id do ator , nome , id do filme e título do filme , usando as tabelas actor , film_actor e film.
      Dica: você precisará fazer mais de um JOIN na mesma query .*/
-     
-     
+SELECT * FROM actor;
+SELECT * FROM film_actor;
+SELECT * FROM film;
+
+SELECT
+	a.actor_id,
+	a.first_name,
+    f.film_id,
+    f.title
+FROM
+	actor AS a
+INNER JOIN
+	film_actor AS fa ON fa.actor_id = a.actor_id
+INNER JOIN
+	film AS f ON f.film_id = fa.film_id;
