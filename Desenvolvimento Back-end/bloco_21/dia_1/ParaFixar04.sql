d8214321fda08d01bd0052fbf1ed49fe500fbbf5
Rafael Araujo <rafacarlosaraujo@gmail.com>
2023-01-26 19:31:45 -0300

    Para fixar 04

--- Desenvolvimento Back-end/bloco_21/dia_1/ParaFixar04.sql
+++ Desenvolvimento Back-end/bloco_21/dia_1/ParaFixar04.sql
@@ -5,4 +5,4 @@
 SELECT datediff('2030-01-20', CURRENT_DATE());
 
 -- 2 Monte uma query exiba a diferença de horas entre '10:25:45' e '11:00:00'.
-SELECT TIMEDIFF('10:25:45', '11:00:00');;
+SELECT TIMEDIFF('10:25:45', '11:00:00');
