68fef4d1bfe62a388093fd912b744f4d3fdaa680
Rafael Araujo <rafacarlosaraujo@gmail.com>
2023-03-26 15:47:52 -0300

    Praticando Group By - 02

--- Desenvolvimento Back-end/bloco_21/dia_1/Praticando GROUP BY.sql
+++ Desenvolvimento Back-end/bloco_21/dia_1/Praticando GROUP BY.sql
@@ -51,3 +51,49 @@ GROUP BY rating;
 SELECT rating, SUM(replacement_cost)
 FROM sakila.film
 GROUP by rating;
+
+
+SELECT first_name, COUNT(*)
+FROM sakila.actor
+GROUP BY first_name
+HAVING COUNT(*) > 2;
+
+-- Ou, melhor ainda, usando o AS para dar nomes às colunas de agregação,
+-- melhorando a leitura do resultado
+SELECT first_name, COUNT(*) AS nomes_cadastrados
+FROM sakila.actor
+GROUP BY first_name
+HAVING nomes_cadastrados > 2;
+
+-- Observação: o alias não funciona com strings para o HAVING,
+-- então use o underline ("_") para separar palavras
+-- Ou seja, o exemplo abaixo não vai funcionar
+SELECT first_name, COUNT(*) AS 'nomes cadastrados'
+FROM sakila.actor
+GROUP BY first_name
+HAVING 'nomes cadastrados' > 2;
+
+SELECT rating, rental_rate, COUNT(1) as total FROM sakila.film
+GROUP BY rental_rate, rating
+HAVING total < 70
+ORDER BY rating, rental_rate;
+
+-- Para fixar
+-- 1. Usando a query a seguir, modifique-a de forma que exiba apenas as durações médias que estão entre 115.0 a 121.50. 
+-- Além disso, dê um alias (apelido) à coluna gerada por AVG(length), de forma que deixe a query mais legível. 
+-- Finalize ordenando os resultados de forma decrescente.
+SELECT rating, AVG(length) AS duracao_media
+FROM sakila.film
+GROUP BY rating
+HAVING duracao_media > 115.0 and duracao_media < 121.51
+ORDER BY duracao_media DESC;
+
+
+-- 2. Usando a query a seguir, exiba apenas os valores de total do custo de substituição que estão acima de $3950.50. 
+-- Dê um alias que faça sentido para SUM(replacement_cost), de forma que deixe a query mais legível. 
+-- Finalize ordenando os resultados de forma crescente.
+SELECT rating, SUM(replacement_cost) as total_custo_substituicao
+FROM sakila.film
+GROUP by rating
+HAVING total_custo_substituicao > 3950.50
+ORDER By total_custo_substituicao ASC;
